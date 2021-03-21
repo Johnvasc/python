145eda960dba5062d5f07e254444b84eeff5b947
John Vasconcelos <39773960+Johnvasc@users.noreply.github.com>
2021-03-20 22:34:27 -0300

    Create Lagrange_poli.py

---
+++ Lagrange_poli.py
@@ -0,0 +1,22 @@
+import numpy as np
+def Lagrange():
+    v = np.poly1d([1,0])
+    pts = int(input("insira a quantidade de pontos\n"))
+    x, y = [], []
+    for i in range(pts):
+        var = float(input("insira o x \n"))
+        x.append(var)
+        var = float(input("insira o y relativo ao x\n"))
+        y.append(var)
+    coefs = []
+    for n in range(pts):
+        resul = 1
+        for j in range(pts):
+            if n!=j:
+                resul *= ((v-x[j])/(x[n]-x[j]))
+        coefs.append(resul)
+    resul = 0
+    for n2 in range(pts):
+        resul = resul + y[n2]*coefs[n2]
+    print("o resultado da interpolacao: ", resul);
+Lagrange()
